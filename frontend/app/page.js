26429455d01bce48742dabe396cc59ce34881158
viktor-morhun <morhun.fe@gmail.com>
2026-02-23 13:54:11 +0200

    redeploy check

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -106,7 +106,7 @@ export default function Home() {
           <input
             className={styles.input}
             type="text"
-            placeholder="Add a new task..."
+            placeholder="Add a new task"
             value={newTitle}
             onChange={(e) => setNewTitle(e.target.value)}
           />
